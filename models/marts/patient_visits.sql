7ac3445c24262898065c5284d0d34e3d83c6ee92
bfreiheit <belinda.freiheit@gmail.com>
2025-08-08 17:53:48 +0200

    clean sql reference

--- models/marts/patient_visits.sql
+++ models/marts/patient_visits.sql
@@ -1,14 +1,14 @@
 select
-    date_trunc(visit_datetime, month) as date_month,
-    diagnosis,
-    pet_type,
-    breed,
-    gender,
-    count(visit_id) as cnt_vists,
-    sum(med_cost) as sum_med_cost,
-    date_diff(max(visit_datetime), min(date_registration), day) as visit_duration,
-    avg(med_dosage) as avg_med_dosage,
-    avg(patient_age) as avg_patient_age
+    date_trunc(vst.visit_datetime, month) as date_month,
+    vst.diagnosis,
+    crd.pet_type,
+    crd.breed,
+    crd.gender,
+    count(vst.visit_id) as cnt_vists,
+    sum(vst.med_cost) as sum_med_cost,
+    date_diff(max(vst.visit_datetime), min(crd.date_registration), day) as visit_duration,
+    avg(vst.med_dosage) as avg_med_dosage,
+    avg(crd.patient_age) as avg_patient_age
 
 from {{ ref('stg_visits') }} as vst
 left join {{ ref('stg_reg_cards') }} as crd on vst.patient_id = crd.patient_id
